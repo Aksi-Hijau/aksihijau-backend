f4daaf20913f6761292eef28872b27dbd430dab3
Juan Angela Alma <juanlah77@gmail.com>
2023-06-11 20:05:01 +0700

    [Feat] Soil: add api fetch soils by name

--- src/controllers/soil.controller.js
+++ src/controllers/soil.controller.js
@@ -17,8 +17,33 @@ const getSoilByIdHandler = async (req, res) => {
   }
 }
 
+const getSoilsHandler = async (req, res) => {
+  try {
+    const { name: type } = req.query
+
+    if (type) {
+      const soil = await SoilService.findSoilByQuery({ type })
+
+      if (!soil) {
+        return res.status(404).send(createApiResponse(false, null, { name: "Soil not found" }))
+      }
+
+      return res.send(createApiResponse(true, soil, null))
+    }
+
+    const soils = await SoilService.findSoilsByQuery({})
+
+    const updatedSoils = soils.map(({ id, type }) => ({ id, type } ))
+
+    return res.status(200).send(createApiResponse(true, updatedSoils, null))
+  } catch (error) {
+    return res.status(500).send(createApiResponse(false, null, error.message))
+  }
+}
+
 const SoilController = {
   getSoilByIdHandler,
+  getSoilsHandler
 }
 
 module.exports = SoilController;
--- src/services/soil.service.js
+++ src/services/soil.service.js
@@ -2,16 +2,22 @@ const Soil = require("../models").Soil;
 
 const findSoilByQuery = async (query) => {
   return Soil.findOne({ where: query });
-}
+};
 
 const checkSoilId = async (soilId) => {
   const soil = await findSoilByQuery({ id: soilId });
   return soil !== null;
-}
+};
+
+const findSoilsByQuery = async (query) => {
+  return Soil.findAll({ where: query });
+};
 
 const SoilService = {
   findSoilByQuery,
-  checkSoilId
-}
+  checkSoilId,
+  findSoilsByQuery,
+  findSoilByQuery
+};
 
 module.exports = SoilService;
