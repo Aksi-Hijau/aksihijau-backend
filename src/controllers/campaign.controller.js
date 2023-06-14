ca412730b2bdbe14a234986b6271f3d7649b7f0f
Juan Angela Alma <juanlah77@gmail.com>
2023-06-14 15:35:10 +0700

    [Fix] format date in reports

--- src/controllers/campaign.controller.js
+++ src/controllers/campaign.controller.js
@@ -197,7 +197,7 @@ const getReportsHandler = async (req, res) => {
         creatorImage: report.user.photo,
         title: report.title,
         body: report.body,
-        createdAt: report.createdAt,
+        createdAt: formattedDate(report.createdAt),
       };
     });
 
